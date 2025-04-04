5a85242ce3c16b8ca85bbd19639f1c775f2b7041
Gabriel K <gab270b@gmail.com>
2025-04-04 11:51:13 +0200

    clean import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import AboutSummary from "@/components/AboutSummary";
 import Footer from "@/components/Footer";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 import Capabilities from "@/components/Capabalities";
